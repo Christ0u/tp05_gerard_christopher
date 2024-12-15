1de35092dc696c3f0fddb096296f3edb22c96f30
Christopher GERARD <christopher.gerard@ikmail.com>
2024-12-15 12:29:30 +0100

    CHG : Modification des fichiers racines pour afficher les composants header, footer et products-list
    Signed-off-by: Christopher GERARD <christopher.gerard@ikmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ProductsListComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tp05_gerard_christopher';
+
+  inStock: boolean = false;
+  maxPrice: number = 100;
+  minRating: number = 0;
+
+  onInStockChange(value: boolean) {
+    this.inStock = value;
+  }
+
+  onMaxPriceChange(value: number) {
+    this.maxPrice = value;
+  }
+
+  onMinRatingChange(value: number)
+  {
+    this.minRating = value;
+  }
 }
